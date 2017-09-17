46ad0d7fdac3a03d3a6050c97eacf4829d23f385
Perdog <jacob_perry@hotmail.ca>
2017-09-17 08:54:12 -0400

    Forgot to localize webpage

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ function waitForIt() {
 		
 		var willPay = 0;
 		var totalm3 = 0
-		var finalText = "<h4>What I'll buy:</h4><br><br><table style=\"width:100%>\" <tr> <th>Item name</th><th>Price per unit</th><th>Total</th></tr>";
+		var finalText = "<h4>What I'll buy:</h4><br><br><table style=\"width:100%>\" <tr> <th>Item name</th><th>Quantity</th><th>Price per unit</th><th>Total</th></tr>";
 		var messageBody = "Someone wants to sell you shit, bruh! \n\n";
 		//messageBody += "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><title></title><style></style></head><body>";
 		//messageBody += "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" height=\"100%\" width=\"100%\" id=\"bodyTable\"><tr><td align=\"center\" valign=\"top\"><table border=\"0\" cellpadding=\"20\" cellspacing=\"0\" width=\"600\" id=\"emailContainer\">";
@@ -108,7 +108,7 @@ function waitForIt() {
 			
 			messageBody += k.item_name + "\t\t x" + k.quantity.toLocaleString() + "\t\t @ " + k.buy_average.toLocaleString(undefined, { minimumFractionDigits:2}) + " isk/unit\t\tTotal: " + k.will_pay.toLocaleString(undefined, { minimumFractionDigits:2}) + " isk\t\t" + perm3.toLocaleString(undefined, { minimumFractionDigits:2}) + " isk per m3 \n";
 			//messageBody += "<tr><td>" + k.item_name + "</td><td>" + k.quantity + "</td><td>" + k.buy_average + "</td><td>" + k.will_pay + "</td><td>" + perm3 + "</td></tr>";
-			finalText += "<tr><td>" + k.item_name + "</td><td>" + k.buy_average + " isk</td><td>" + k.will_pay + " isk</td></tr>"
+			finalText += "<tr><td>" + k.item_name + "</td><td>" + k.quantity.toLocaleString() + "</td><td>" + k.buy_average.toLocaleString(undefined, { minimumFractionDigits:2}) + " isk</td><td>" + k.will_pay.toLocaleString(undefined, { minimumFractionDigits:2}) + " isk</td></tr>"
 		}
 		
 		messageBody += "\nTotal Profit per m3: " + totalm3;
